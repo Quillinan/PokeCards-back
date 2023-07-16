d48101f37e23454c71164b24b943ba8498f235ca
Quillinan <milton.quillinan@gmail.com>
2023-07-16 18:16:49 -0300

    removeFromCart + getCardsOnCart + getActiveCarts

--- src/controllers/CartController.js
+++ src/controllers/CartController.js
@@ -65,6 +65,77 @@ const cartController = {
       res.status(500).json({ error: error.message });
     }
   },
+  removeFromCart: async (req, res) => {
+    try {
+      const token = req.headers.authorization?.replace("Bearer ", "");
+      const cardId = req.body.cardId;
+
+      const Carts = db.collection("carts");
+      const Cards = db.collection("cards");
+
+      const cart = await Carts.findOne({ token });
+
+      if (!cart) {
+        return res.status(404).json({ error: "Carrinho não encontrado" });
+      }
+
+      const card = await Cards.findOne({ _id: new ObjectId(cardId) });
+
+      if (!card) {
+        return res.status(404).json({ error: "Card não encontrado" });
+      }
+
+      await Carts.updateOne({ _id: cart._id }, { $pull: { cards: card._id } });
+
+      res
+        .status(200)
+        .json({ message: "Carta removida do carrinho com sucesso" });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+  getCardsOnCart: async (req, res) => {
+    try {
+      const token = req.headers.authorization?.replace("Bearer ", "");
+
+      const Carts = db.collection("carts");
+      const Cards = db.collection("cards");
+
+      const cart = await Carts.findOne({ token });
+
+      if (!cart) {
+        return res.status(404).json({ error: "Carrinho não encontrado" });
+      }
+
+      const cardIds = cart.cards.map((card) => card._id);
+
+      const cards = await Cards.find({ _id: { $in: cardIds } }).toArray();
+
+      res.status(200).json({ cards });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+  getCarts: async (req, res) => {
+    try {
+      const userId = req.user._id;
+      const Carts = db.collection("carts");
+
+      const carts = await Carts.find().toArray();
+      const validCarts = [];
+
+      for (const cart of carts) {
+        const decodedToken = jwt.verify(cart.token, secretKey);
+        if (decodedToken.id == userId && cart.cards) {
+          validCarts.push(cart);
+        }
+      }
+
+      res.status(200).json({ carts: validCarts });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
 };
 
 export default cartController;
