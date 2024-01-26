6b0d1e725d268603c677b0adf44be3fb01733cd8
Quillinan <milton.quillinan@gmail.com>
2024-01-26 18:42:11 -0300

    fix: controller names

--- src/routers/cartRoutes.js
+++ src/routers/cartRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import verifyToken from "../middlewares/AuthMiddleware.js";
-import cartController from "../controllers/CartController.js";
+import cartController from "../controllers/cartController.js";
 
 const cartRoutes = express.Router();
 
